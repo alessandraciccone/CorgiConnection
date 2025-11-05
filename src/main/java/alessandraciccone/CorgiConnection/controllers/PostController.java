78a9d8fb2608af44c6bb8d535c590230cb63677b
alessandraciccone <alessandraciccone@icloud.com>
2025-11-05 11:26:49 +0100

    adds CorgiController

--- src/main/java/alessandraciccone/CorgiConnection/controllers/PostController.java
+++ src/main/java/alessandraciccone/CorgiConnection/controllers/PostController.java
@@ -1,12 +1,159 @@
 package alessandraciccone.CorgiConnection.controllers;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import alessandraciccone.CorgiConnection.payloads.PostDTO;
+import alessandraciccone.CorgiConnection.payloads.PostResponseDTO;
+import alessandraciccone.CorgiConnection.payloads.PostUpdateDTO;
+import alessandraciccone.CorgiConnection.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/posts")
 public class PostController {
 
-    
+    @Autowired
+    PostService postService;
+
+    //CREO UN POST //body json {
+    //  "content": "Testo del post",
+    //  "author_ID": "uuid-dell-autore",
+    //  "corgi_Id": "uuid-del-corgi-opzionale"
+    //}
+    //http://localhost:3001/posts
+
+
+    @PostMapping
+    public ResponseEntity<PostResponseDTO> createPost(@RequestBody PostDTO postDTO) {
+        return ResponseEntity.ok(postService.createPost(postDTO));
+    }
+
+
+    //TROVO POST PER ID GET http://localhost:3001/posts/{id}
+    @GetMapping("/{id}")
+    public ResponseEntity<PostResponseDTO> getPostById(@PathVariable UUID id) {
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+
+    //AGGIORNO POST PUT http://localhost:3001/posts/{id}
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PostResponseDTO> updatePost(
+            @PathVariable UUID id,
+            @RequestBody PostUpdateDTO postUpdateDTO
+            ){
+        return ResponseEntity.ok(postService.updatePost(id, postUpdateDTO));
+    }
+
+    //CANCELLO UN POST DELETE http://localhost:3001/posts/{id}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable UUID id){
+        postService.deletePost(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    // TROVO I POST PAGINATI
+    //GET http://localhost:3001/posts?page=0&size=10&sortBy=datePost
+
+    @GetMapping
+    public ResponseEntity<Page<PostResponseDTO>> getAllPosts(
+            @RequestParam( defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "datePost") String sortBy){
+        return ResponseEntity.ok(postService.getAllPosts(page,size,sortBy));
+    }
+
+    // TROVO PER AUTORE
+
+    //GET http://localhost:3001/posts/author/{authorId}
+
+    @GetMapping("/author/{authorId}")
+    public ResponseEntity<List<PostResponseDTO>> getPostsByAuthor(@PathVariable UUID authorId) {
+        return ResponseEntity.ok(postService.getsPostByAuthor(authorId));
+    }
+
+    //TROVO POST PERDATA
+//GET http://localhost:3001/posts/date/{date}
+    @GetMapping("/date/{date}")
+    public ResponseEntity<List<PostResponseDTO>> getPostsByDate(@PathVariable String date ){
+        LocalDate localDate= LocalDate.parse(date);
+        return ResponseEntity.ok(postService.getPostsbyDate(localDate));
+    }
+
+
+    //Ricerca post con filtri
+//GET http://localhost:3001/posts/search?author_id={uuid}&authorUsername=Pippo&contentKeyword=gioco&page=0&size=10&sortBy=datePost
+
+    @GetMapping("/search")
+    public ResponseEntity<Page<PostResponseDTO>> searchPosts(
+            @RequestParam(required = false) UUID author_id,
+            @RequestParam(required = false) String authorUsername,
+            @RequestParam(required = false) String authorFirstName,
+            @RequestParam(required = false) String authorLastName,
+            @RequestParam(required = false) String authorCity,
+            @RequestParam(required = false) String contentKeyword,
+            @RequestParam(required = false) UUID corgi_Id,
+            @RequestParam(required = false) String corgiName,
+            @RequestParam(required = false) LocalDate exactDate,
+            @RequestParam(required = false) LocalDate dateAfter,
+            @RequestParam(required = false) LocalDate dateBefore,
+            @RequestParam(required = false) LocalDate startDate,
+            @RequestParam(required = false) LocalDate endDate,
+            @RequestParam(required = false) Boolean hasPhotos,
+            @RequestParam(required = false) Boolean hasComments,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "datePost") String sortBy
+    ) {
+        return ResponseEntity.ok(postService.searchPost(
+                author_id, authorUsername, authorFirstName, authorLastName, authorCity,
+                contentKeyword, corgi_Id, corgiName, exactDate, dateAfter, dateBefore,
+                startDate, endDate, hasPhotos, hasComments, page, size, sortBy
+        ));
+    }
+
+//CERCO POST PIù RECENTI(ULTIMI 7 GIORNI)
+    //GET http://localhost:3001/posts/recent
+    @GetMapping("/recent")
+    public ResponseEntity<List<PostResponseDTO>> getrecentPosts(){
+        return  ResponseEntity.ok(postService.getRecentPosts());
+    }
+
+    // CERCO POST CON FOTO
+    //GET http://localhost:3001/posts/photos
+    @GetMapping("/photos")
+    public ResponseEntity<List<PostResponseDTO>> getPostsWithPhotos(){
+        return ResponseEntity.ok(postService.getPostsWithPhotos());
+    }
+
+
+    //CERCO POST PER CITTA
+    //GET http://localhost:3001/posts/city?city=Milano
+    @GetMapping("/city")
+    public ResponseEntity<List<PostResponseDTO>> getPostsByAuthorCity(@RequestParam String city){
+        return ResponseEntity.ok(postService.getPostsByAuthorCity(city));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
