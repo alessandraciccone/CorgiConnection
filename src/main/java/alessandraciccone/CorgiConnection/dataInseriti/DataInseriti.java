a8c2da86c8c6aa8b53aa8dc0a9dd64a73db3958a
alessandraciccone <alessandraciccone@icloud.com>
2025-11-07 09:04:47 +0100

    adds comment class

---
+++ src/main/java/alessandraciccone/CorgiConnection/dataInseriti/DataInseriti.java
@@ -0,0 +1,16 @@
+
+/*
+package alessandraciccone.CorgiConnection.dataInseriti;
+
+
+ArtAndHop
+token eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI4ODEyOWIzZC02NTQxLTRkZWItODU4Ny1kNzhjMzI5NGZiNTQiLCJpYXQiOjE3NjI0NTUwODksImV4cCI6MTc5Mzk5MTA4OX0.NYi2LwlZxOIgbSlOBCkJvyLkbHDirKKq2TTGzchXfyA
+password ciao333
+email userino@mail.com
+id 88129b3d-6541-4deb-8587-d78c3294fb54
+
+cane id 2da1f5b8-1c2f-4ed4-8f76-860532bdc99c
+
+
+
+*/
