6d9237fcbe7fed8ec6778cdf0a09e6bc9e2749d7
alessandraciccone <alessandraciccone@icloud.com>
2025-11-10 13:35:54 +0100

    import dependency frontend

--- CorgiConnectionFE/src/main.jsx
+++ CorgiConnectionFE/src/main.jsx
@@ -1,6 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
