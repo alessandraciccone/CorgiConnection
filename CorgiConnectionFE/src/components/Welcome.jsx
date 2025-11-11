6e820a63218f3e30188c24a795ef4f7bd415f889
alessandraciccone <alessandraciccone@icloud.com>
2025-11-11 15:17:01 +0100

    adds welcome page

--- CorgiConnectionFE/src/components/Welcome.jsx
+++ CorgiConnectionFE/src/components/Welcome.jsx
@@ -1,50 +1,26 @@
-import React, { useEffect, useState } from "react";
 import cartooncorgi from "../assets/img/cartooncorgi.png";
 import "../css/Welcome.css";
 
 const Welcome = () => {
-  const [step, setStep] = useState(1);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setStep(2);
-    }, 5000); // dopo 5 secondi
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
-    <div className="container-fluid welcome-wrapper">
-      <div className="row justify-content-center">
-        <div className="col-12 col-md-8 text-center position-relative comic-container">
-          <img
-            className="cartoon-corgi"
-            src={cartooncorgi}
-            alt="Cartoon girl with corgi"
-          />
+    <div className="container-fluid">
+      {/* Immagine unica */}
+      <img
+        className="cartoon-corgi"
+        src={cartooncorgi}
+        alt="Cartoon girl with corgi"
+      />
 
-          {step === 1 && (
-            <>
-              <div className="speech-bubble fade-in">
-                Ciao! Benvenuti su Corgi Connection!
-              </div>
-              <div className="speech-bubble dog fade-in">
-                Io mi chiamo Gaspare, e lei è la mia umana Alessandra!
-              </div>
-            </>
-          )}
+      {/* Fumetto Alessandra */}
+      <div className="speech-bubble main">
+        Ciao! Benvenuti su Corgi Connection, il posto dove i Corgi si
+        incontrano!
+      </div>
 
-          {step === 2 && (
-            <>
-              <div className="speech-bubble fade-in">
-                Qui potrai conoscere nuovi amici e scoprire curiosità sui corgi!
-              </div>
-              <div className="speech-bubble dog fade-in">
-                Registrati e preparati a scodinzolare di gioia!
-              </div>
-            </>
-          )}
-        </div>
+      {/* Fumetto Gaspare */}
+      <div className="speech-bubble dog">
+        Io mi chiamo Gaspare, e lei è la mia umana Alessandra! Registrati e
+        vieni a conoscere nuovi amici insieme a noi ❤️
       </div>
     </div>
   );
